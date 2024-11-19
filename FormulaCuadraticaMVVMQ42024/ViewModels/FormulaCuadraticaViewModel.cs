9b4c23b936158f83b637c8faa89858171646ef0b
gurdian0614 <ivan.gurdian@outlook.com>
2024-11-18 19:18:32 -0600

    Creating ViewModel file and adding observable properties

---
+++ FormulaCuadraticaMVVMQ42024/ViewModels/FormulaCuadraticaViewModel.cs
@@ -0,0 +1,29 @@
+﻿
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace FormulaCuadraticaMVVMQ42024.ViewModels
+{
+    public partial class FormulaCuadraticaViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private double a;
+
+        [ObservableProperty]
+        private double b;
+
+        [ObservableProperty]
+        private double c;
+
+        [ObservableProperty]
+        private double x1;
+
+        [ObservableProperty]
+        private double x2;
+
+        private void Alerta(string Titulo, string Mensaje)
+        {
+            MainThread.BeginInvokeOnMainThread(async () => await App.Current!.MainPage!.DisplayAlert(Titulo, Mensaje, "Aceptar"));
+        }
+
+    }
+}
