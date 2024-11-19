1a30de9157a64c432df6bfe90e8ba6561646873d
gurdian0614 <ivan.gurdian@outlook.com>
2024-11-18 18:55:34 -0600

    Changing view

--- FormulaCuadraticaMVVMQ42024/App.xaml.cs
+++ FormulaCuadraticaMVVMQ42024/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace FormulaCuadraticaMVVMQ42024
+﻿using FormulaCuadraticaMVVMQ42024.Views;
+
+namespace FormulaCuadraticaMVVMQ42024
 {
     public partial class App : Application
     {
@@ -6,7 +8,7 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new FormulaCuadraticaView();
         }
     }
 }
