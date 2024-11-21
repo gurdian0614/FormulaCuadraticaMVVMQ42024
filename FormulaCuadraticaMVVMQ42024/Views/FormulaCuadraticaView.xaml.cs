471ba934d011ba6ee5235412f21633cb0e6b0186
gurdian0614 <ivan.gurdian@outlook.com>
2024-11-20 19:23:17 -0600

    Building view

--- FormulaCuadraticaMVVMQ42024/Views/FormulaCuadraticaView.xaml.cs
+++ FormulaCuadraticaMVVMQ42024/Views/FormulaCuadraticaView.xaml.cs
@@ -1,9 +1,14 @@
+using FormulaCuadraticaMVVMQ42024.ViewModels;
+
 namespace FormulaCuadraticaMVVMQ42024.Views;
 
 public partial class FormulaCuadraticaView : ContentPage
 {
+	FormulaCuadraticaViewModel ViewModel;
 	public FormulaCuadraticaView()
 	{
 		InitializeComponent();
+		ViewModel = new FormulaCuadraticaViewModel();
+		BindingContext = ViewModel;
 	}
 }
